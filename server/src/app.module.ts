5846b636ccd376f568bed656431d8e795dbb5066
VasilyMamaev <mamaev.vasily@gmail.com>
2021-02-27 21:07:45 +0500

    rest api and dto for track and comment entities

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [TrackModule],
+  imports: [
+    TrackModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.gmoih.mongodb.net/music-platform?retryWrites=true&w=majority',
+    ),
+  ],
 })
 export class AppModule {}
